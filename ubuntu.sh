d8803b7ae5a1cbf3d638cb77e278afdba416dcc5
Laure <sandralaure16@gmail.com>
2022-10-25 15:35:13 -0700

    agent-scripts

---
+++ ubuntu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+  #Author: Laure
+  #Date: 25-10-2022
+  #This file is to install packages on ubuntu
+
+apt install wget -y
+apt install net-utils -y
+apt install sysstat -y
+apt install finger -y
+apt install gcc -y
+apt install make -y
+apt install python3 -y
+apt install epel-release -y
+apt install git -y
+sleep 10
+echo "Installation successfull!"
+echo "go to the next step."
